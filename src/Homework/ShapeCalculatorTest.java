3867c6e1e27b43536d7530dcc81c7c2cb196b76a
BinkaArushankina <albinarushankina@gmail.com>
2023-03-27 09:52:10 +0200

    Homework 21.03.23 interface comparable compareTo HOMEWORK SORTING JUnit 23.03.23 ShapeCalculator, ShapeCalculatorTest

--- src/Homework/ShapeCalculatorTest.java
+++ src/Homework/ShapeCalculatorTest.java
@@ -6,15 +6,44 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShapeCalculatorTest {
     ShapeCalculator shapeCalculator;
+    Area rectangle=new Rectangle("Rectangle",5,10);
+    Area square=new Square("Square",7);
+    Area circle=new Circle("Circle",10);
+    Area[] shapes={circle,rectangle,square};
+    List<Area> shapeList=Arrays.asList(shapes);  //schtobi ne propisiwatj sanowo List erenimaem dannie is massiwa w list  Arrays.asList;
 
     @BeforeEach
     void  init(){
         shapeCalculator=new ShapeCalculator();
+
     }
+    @Test
+    public void test_sorted(){
+        Area[]expected ={square,rectangle,circle} ;
+        List<Area> expectedList= Arrays.asList(expected);
+
+        Arrays.sort(shapes);
+        Collections.sort(shapeList);
+
+        assertArrayEquals(expected,shapes);
+        assertEquals(expectedList,shapeList);
+    }
+    @Test
+    public void  test_sorted_one(){
+        Area[]expected= {circle};
+        Area[]actual= {circle};
+        Arrays.sort(actual);
+
+        assertArrayEquals(expected,actual);
+    }
+
     @Test
     public void test_two_shapes(){
         Area[]shapes={new Circle("Circle",10),new Rectangle("Rectangle",5,10)};
